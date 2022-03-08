51e7a5a46e5f008ea30b7d6c0a1464af5173ad23
Anastasiia Antipina <ananastasiia13@gmail.com>
2022-03-08 04:14:36 +0300

    1st review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,20 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Main Page')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        }
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    return HttpResponse('Grouped Posts')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        }
+    return render(request, 'posts/group_list.html', context)
